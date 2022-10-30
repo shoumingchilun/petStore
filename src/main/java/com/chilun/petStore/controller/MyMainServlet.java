622799cefa2c08eed9fba60c3b8bab34153e99af
齿轮 <2265501251@qq.com>
2022-10-30 16:21:20 +0800

    写了mainServlet

---
+++ src/main/java/com/chilun/petStore/controller/MyMainServlet.java
@@ -0,0 +1,36 @@
+package com.chilun.petStore.controller;
+
+import com.chilun.petStore.pojo.User;
+import com.chilun.petStore.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @auther 齿轮
+ * @create 2022-10-30-15:19
+ * 网站入口
+ */
+
+public class MyMainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("loginUser");
+        if(user==null){
+            req.getRequestDispatcher("/WEB-INF/mainJsp/NoUserMain.jsp").forward(req,resp);
+        }else{
+            req.getRequestDispatcher("UserMain.jsp").forward(req,resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
+    }
+}
