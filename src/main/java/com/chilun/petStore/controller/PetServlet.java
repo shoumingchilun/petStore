15a2f1a7f81bac6c01b58a1adb6ea0beee364dbd
齿轮 <2265501251@qq.com>
2022-10-31 22:00:05 +0800

    写了search的逻辑

--- src/main/java/com/chilun/petStore/controller/PetServlet.java
+++ src/main/java/com/chilun/petStore/controller/PetServlet.java
@@ -2,6 +2,7 @@ package com.chilun.petStore.controller;
 
 import com.chilun.petStore.dao.specialDAO.PetDAO;
 import com.chilun.petStore.pojo.Pet;
+import com.chilun.petStore.service.PetService;
 import com.chilun.petStore.view.Page;
 
 import javax.servlet.ServletException;
@@ -15,7 +16,7 @@ import java.io.IOException;
  * @create 2022-10-30-21:45
  */
 public class PetServlet extends HttpServlet {
-
+    PetService service = new PetService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String petIDStr = req.getParameter("petID");
@@ -26,16 +27,22 @@ public class PetServlet extends HttpServlet {
         } catch (NumberFormatException e) {
         }
         Page<Pet> page = (Page<Pet>) req.getSession().getAttribute("page");
-        Pet pet = null;
-        for (Pet pet1: page.getList()){
-            if (pet1.getPetID()==petID){
-                pet = pet1;
-                break;
+        if(page!=null){
+            Pet pet = null;
+            for (Pet pet1: page.getList()){
+                if (pet1.getPetID()==petID){
+                    pet = pet1;
+                    break;
+                }
             }
+            req.setAttribute("pet",pet);
+            req.setAttribute("page",page);
+            req.getRequestDispatcher("/WEB-INF/Pet/Pet.jsp").forward(req,resp);
+        }else{
+            Pet pet = service.getDao().getPetById(petID);
+            req.setAttribute("pet",pet);
+            req.getRequestDispatcher("/WEB-INF/Pet/Pet.jsp").forward(req,resp);
         }
-        req.setAttribute("pet",pet);
-        req.setAttribute("page",page);
-        req.getRequestDispatcher("/WEB-INF/Pet/Pet.jsp").forward(req,resp);
     }
 
     @Override
